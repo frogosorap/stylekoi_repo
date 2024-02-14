56f2a5840cb4094da4dc54e0412140d362a96228
Elga J Henry <henryelga2004@gmail.com>
2024-02-14 09:41:35 +0000

    formatted files

--- client/src/components/CarTable.js
+++ client/src/components/CarTable.js
@@ -1,33 +1,34 @@
-import React, {Component} from "react"
-import CarTableRow from "./CarTableRow"
+import React, { Component } from "react";
+import CarTableRow from "./CarTableRow";
 
+export default class CarTable extends Component {
+  render() {
+    return (
+      <div className="itemsRow">
+        {this.props.cars.map((car) => (
+          <CarTableRow key={car._id} car={car} />
+        ))}
+      </div>
+      // <table>
+      //     <thead>
+      //         <tr>
+      //             <th>Name</th>
+      //             <th>Colour</th>
+      //             <th>Year</th>
+      //             <th>Price</th>
+      //             <th>Size</th>
+      //             <th>Gender</th>
+      //             <th>Fabric</th>
+      //             <th>Description</th>
+      //             <th>Photos</th>
+      //             <th></th>
+      //         </tr>
+      //     </thead>
 
-export default class CarTable extends Component 
-{
-    render() 
-    {
-        return (
-            <div className="itemsRow">{this.props.cars.map((car) => <CarTableRow key={car._id} car={car}/>)}</div>
-            // <table>
-            //     <thead>
-            //         <tr>
-            //             <th>Name</th>
-            //             <th>Colour</th>
-            //             <th>Year</th>
-            //             <th>Price</th>
-            //             <th>Size</th>
-            //             <th>Gender</th>
-            //             <th>Fabric</th>
-            //             <th>Description</th>
-            //             <th>Photos</th>
-            //             <th></th>
-            //         </tr>
-            //     </thead>
-                  
-            //     <tbody>
-            //         {this.props.cars.map((car) => <CarTableRow key={car._id} car={car}/>)}
-            //     </tbody>
-            // </table>      
-        )
-    }
+      //     <tbody>
+      //         {this.props.cars.map((car) => <CarTableRow key={car._id} car={car}/>)}
+      //     </tbody>
+      // </table>
+    );
+  }
 }
