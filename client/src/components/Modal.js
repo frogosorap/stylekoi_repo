843b23b2322c050a875fe98f0fbd06b07d60da25
Raphael Frogoso <alkali.smiley@gmail.com>
2024-02-16 10:08:17 +0000

    Modal changes

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -9,14 +9,24 @@ class Modal extends Component {
         return (
           <div className="modal">
             <div className="modal-content">
-              <span className="close" onClick={onClose}>&times;</span>
-              <h1>{car.name}</h1>
-              <p>Price: €{car.price}</p>
-              <p>Gender: {car.gender}</p>
-              <p>Colour: {car.colour}</p>
-              <p>Size: {car.size}</p>
-              <p>Fabric: {car.fabric}</p>
-              <p>Description: {car.description}</p>
+              <div className ="modal-flex">
+
+                <div className="modal-text">
+                  <span className="close" onClick={onClose}>&times;</span>
+                  <h1>{car.name}</h1>
+                  <p>Price: €{car.price}</p>
+                  <p>Gender: {car.gender}</p>
+                  <p>Colour: {car.colour}</p>
+                  <p>Size: {car.size}</p>
+                  <p>Fabric: {car.fabric}</p>
+                  <p>Description: {car.description}</p>
+                </div>
+
+                <div className="modal-image">
+                  
+                </div>
+
+              </div>
             </div>
           </div>
         );
