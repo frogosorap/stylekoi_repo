45dd33d01361bd4f21738a5669661f303b3db10b
Raphael Frogoso <alkali.smiley@gmail.com>
2024-02-28 14:54:20 +0000

    sort by name asc and desc

--- client/src/components/ShirtGrid.js
+++ client/src/components/ShirtGrid.js
@@ -2,6 +2,82 @@ import React, { Component } from "react";
 import CarTableRow from "./CarTableRow";
 import SearchFilter from "./SearchFilter";
 
+class ShirtGrid extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filterValue: "",
+      sortBy: null,
+      sortOrder: "asc", // Default sorting order is ascending
+    };
+
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.handleSort = this.handleSort.bind(this);
+  }
+
+  handleFilterChange(filterValue) {
+    this.setState({ filterValue });
+  }
+
+  handleSort(event) {
+    const selectedOption = event.target.value;
+    const [sortBy, sortOrder] = selectedOption.split("_");
+    this.setState({ sortBy, sortOrder });
+  }
+
+  render() {
+    const { filterValue, sortBy, sortOrder } = this.state;
+    let { cars } = this.props;
+
+    // Apply filter
+    const filteredCars = filterValue
+      ? cars.filter((car) =>
+          car.name.toLowerCase().includes(filterValue.toLowerCase())
+        )
+      : cars;
+
+    // Apply sorting
+    let sortedCars = [...filteredCars];
+    if (sortBy) {
+      sortedCars = sortedCars.sort((a, b) => {
+        const aValue = a[sortBy];
+        const bValue = b[sortBy];
+        if (sortOrder === "asc") {
+          return aValue.localeCompare(bValue);
+        } else {
+          return bValue.localeCompare(aValue);
+        }
+      });
+    }
+
+    return (
+      <div>
+        <div className="filter-container">
+          <SearchFilter
+            onFilterChange={this.handleFilterChange}
+            filterValue={filterValue}
+          />
+          <div className="sort-dropdown">
+            <select id="sort" onChange={this.handleSort}>
+              <option value="">-- Sort by: --</option>
+              <option value="name_asc">Name by (Ascending)</option>
+              <option value="name_desc">Name by (Descending)</option>
+            </select>
+          </div>
+        </div>
+        <div className="itemsGrid women_section">
+          {sortedCars.map((car) => (
+            <CarTableRow key={car._id} car={car} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShirtGrid;
+
 // export default class ShirtGrid extends Component {
 //   constructor(props) 
 //   {
@@ -62,41 +138,3 @@ import SearchFilter from "./SearchFilter";
 
 // ShirtGrid.js
 
-export default class ShirtGrid extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      filterValue: "",
-    };
-
-    this.handleFilterChange = this.handleFilterChange.bind(this);
-  }
-
-  handleFilterChange(filterValue) {
-    this.setState({ filterValue });
-  }
-
-  render() {
-    const { filterValue } = this.state;
-    const filteredShirts = filterValue
-      ? this.props.cars.filter((car) =>
-          car.name.toLowerCase().includes(filterValue.toLowerCase())
-        )
-      : this.props.cars;
-
-    return (
-      <div>
-        <SearchFilter
-          onFilterChange={this.handleFilterChange}
-          filterValue={filterValue}
-        />
-        <div className="itemsGrid women_section">
-          {filteredShirts.map((car) => (
-            <CarTableRow key={car._id} car={car} />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
