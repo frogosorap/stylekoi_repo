ef0fc35ca0e74510786d4274011c5e00e33ec388
Raphael Frogoso <alkali.smiley@gmail.com>
2024-02-29 13:34:24 +0000

    admin working

--- client/src/components/DisplayAllUsers.js
+++ client/src/components/DisplayAllUsers.js
@@ -21,7 +21,7 @@ export default class DisplayAllUsers extends Component
     
       componentDidMount() {
         axios
-          .get(`${SERVER_HOST}/users`)
+          .get(`${SERVER_HOST}/users/all`)
           .then((res) => {
             this.setState({ users: res.data });
           })
